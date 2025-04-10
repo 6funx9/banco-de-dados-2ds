c2c4f5407aafed57d213ccc21bf61054316fceaa
6funx9 <geovanni.araujo2008gta@gmail.com>
2025-04-09 23:45:12 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Clientes 
+( 
+); 
+
+CREATE TABLE festas 
+( 
+); 
+
+CREATE TABLE pix 
+( 
+ codigo_pix INT,  
+); 
+
+CREATE TABLE pagamentos 
+( 
+); 
+
+CREATE TABLE cartaocredito 
+( 
+ cpf INT,  
+ nome do cartão INT,  
+ endereco INT,  
+); 
+
+CREATE TABLE boleto 
+( 
+ Cod_barras INT,  
+ data_vencimento INT,  
+); 
+
